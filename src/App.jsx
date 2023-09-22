0698563e566de164950d29856660b827532c2518
ASRAF JAHAN <webasraf3@gmail.com>
2023-09-22 16:45:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './App.css'
+// import Grandpa from './components/Grandpa/Grandpa'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
+// import RefForm from './components/RefForm/RefForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
+// import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+  const handleSignUpSubmit = data =>{ 
+    console.log('sign up data', data);
+}
+
+const handleUpdateProfile = data =>{
+  console.log('update Profile', data);
+}
+
+  return (
+    <>
+      
+      <h1>Form Master</h1>
+      {/* <Grandpa></Grandpa> */}
+
+     {/* <SimpleForm></SimpleForm> */}
+     {/* <StatefulForm></StatefulForm> */}
+     {/* <RefForm></RefForm> */}
+     {/* <HookForm></HookForm> */}
+     <ReusableForm 
+     formTitle={'Sign Up'} 
+     handleSubmit={handleSignUpSubmit}>
+      <div>
+        <h2>Sign Up</h2>
+        <p>please sign up right now</p>
+      </div>
+     </ReusableForm>
+     <ReusableForm 
+     formTitle={'Profile Update'}  
+     handleSubmit={handleUpdateProfile} submitBtnText='Update'>
+      <div>
+        <h2>Update Profile</h2>
+        <p>Always keep your profile update</p>
+      </div>
+     </ReusableForm>
+    </>
+  )
+}
+
+export default App
